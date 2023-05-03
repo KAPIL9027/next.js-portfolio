07a46ee47458143ebfe8ea018bdedc90b266f1af
Kapil <sharmaakapil01@gmail.com>
2023-05-03 14:23:04 +0530

    created the intro part with animation using framer and tailwindcss

---
+++ components/BackgroundCircles.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+type Props = {}
+
+function BackgroundCircles({}: Props) {
+  return (
+    < motion.div 
+     initial={{
+        opacity: 0,
+     }}
+     animate={{
+        scale: [1,2,2,3,1],
+        opacity: [0.1,0.2,0.4,0.8,0.1,1.0],
+        borderRadius: ["20%","20%","50%","80%","20%"]
+     }}
+     transition={{
+        delay: 2.5
+     }}
+    className="relative flex justify-center items-center">
+        <div className="border border-[#333333]
+        rounded-full sm:h-[200px] w-[200px] 
+        animate-ping absolute mt-52"/>
+        <div className="border border-[#333333]
+        rounded-full h-[300px] w-[300px]
+        absolute mt-52
+        "/>
+        <div className="border border-[#333333]
+        rounded-full h-[400px] w-[400px] 
+        absolute mt-52
+        "/>
+        <div className="border border-[#F7AB0A]
+        rounded-full h-[500px] w-[500px] opacity-20 
+        absolute mt-52
+        animate-pulse"/>
+        <div className="border border-[#333333]
+        rounded-full h-[600px] w-[600px] 
+        absolute mt-52
+        "/>
+    </motion.div>
+  )
+}
+
+export default BackgroundCircles
---
+++ components/Hero.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Cursor,useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles'
+type Props = {}
+
+export default function Hero({}: Props) {
+    const [text,count] = useTypewriter({
+        words: [
+           "Hi, The Name's Kapil",
+           "MERN Stack Developer",
+           `Aws Certified Cloud 
+           Practitioner`
+        ],
+        loop: true,
+        delaySpeed: 2000
+    })
+  return (
+    <div className="h-screen flex flex-col
+    space-y-8 items-center justify-center
+    text-center">
+         <BackgroundCircles />
+         <img  alt="profile-image"
+          src="https://user-images.githubusercontent.com/76560065/235853202-baff50d7-1d4a-4df8-b5cc-50f1aca7b791.png"
+          className="relative rounded-full w-32 h-32 mx-auto object-cover"/>
+        <div className="z-10">
+        <h2 className="text-sm uppercase
+        text-gray-500 pb-2 tracking-[15px]">
+            CSE Undergrad
+        </h2>
+        <h1 className=" text-3xl sm:text-5xl font-semibold
+        px-10">
+        <span className="">{text}</span>
+    <Cursor cursorColor="#F7ABBA"/>
+        </h1>
+        </div>
+        <div>
+            <button className="heroButton">About</button>
+            <button className="heroButton">Experience</button>
+            <button className="heroButton">Skills</button>
+            <button className="heroButton">Projects</button>
+        </div>
+    </div>
+   
+  )
+}
