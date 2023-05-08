b2683f7d10012cfc9e5596f2de724aa8d1cb1b07
Kapil <sharmaakapil01@gmail.com>
2023-05-08 16:31:47 +0530

    fixed the hover skill ui issue

--- components/Skills.tsx
+++ components/Skills.tsx
@@ -23,7 +23,7 @@ export default function Skills({skills}: Props) {
   justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px]
   text-gray-500 text-2xl ">Skills</h3>
-        <h3 className="text-[0.5rem] sm:text-[1rem] absolute top-36 uppercase tracking-[3px]
+        <h3 className="text-[6px] sm:text-[1rem] absolute top-36 uppercase tracking-[3px]
         text-gray-500 text-sm">
             Hover over a skill for current proficiency
         </h3>
